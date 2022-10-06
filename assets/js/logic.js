6e73d122c584742b73662f89cc9097dd5f0f12fd
Jordan Colubiale <jordancolubiale@Jordans-Air-2.attlocal.net>
2022-10-06 17:39:16 -0400

    index.html complete

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,11 @@
 // variables to keep track of quiz state
-    // currentQuestion
-    var currentQuestion = 0
-    // time
-    var timeCounter = questions.length*10
-    // timerId
-    var timerId;
-  var score = 0
+// currentQuestion
+var currentQuestion = 0
+// time
+var timeCounter = questions.length * 10
+// timerId
+var timerId;
+var score = 0
 // variables to reference DOM elements
 var questionsEl = document.getElementById('questions');
 var endScreen = document.getElementById("end-screen")
@@ -16,13 +16,16 @@ var answerB = document.getElementById("answer-b")
 var answerC = document.getElementById("answer-c")
 var feedback = document.getElementById("choicefeedback")
 var timerEl = document.querySelector(".timenum")
-questionsEl.style.display ="none"
-endScreen.style.display ="none"
+var submitButton = document.getElementById("submit")
+var highscore = document.getElementById("feedback")
+questionsEl.style.display = "none"
+endScreen.style.display = "none"
+highscore.style.display = "none"
 /// FUNCTION TO START THE QUIZ
 function startQuiz() {
   // hide start screen 
-  questionsEl.style.display ="block"
-  startButton.style.display ="none"
+  questionsEl.style.display = "block"
+  startButton.style.display = "none"
   // un-hide questions section
 
   // start timer
@@ -35,41 +38,42 @@ function startQuiz() {
 /// FUNCTION TO GET/SHOW EACH QUESTION ///
 function getQuestion() {
   // get current question object from array
-questionTitle.textContent = questions[currentQuestion].question
-answerA.textContent = questions[currentQuestion].answers.a
-answerB.textContent = questions[currentQuestion].answers.b
-answerC.textContent = questions[currentQuestion].answers.c
+  questionTitle.textContent = questions[currentQuestion].question
+  answerA.textContent = questions[currentQuestion].answers.a
+  answerB.textContent = questions[currentQuestion].answers.b
+  answerC.textContent = questions[currentQuestion].answers.c
   // update title with current question
 
   // clear out any old question choices
 
   // loop over choices
-    // FOR {
-      // create new button for each choice
-  
-      // display on the page
-      
-    // } 
+  // FOR {
+  // create new button for each choice
+
+  // display on the page
+
+  // } 
 }
 
 /// FUNCTION FOR CLICKING A QUESTION ///
 function questionClick(event) {
-var userChoice = event.target.getAttribute("id").split("-")[1]
-console.log(userChoice)
+  var userChoice = event.target.getAttribute("id").split("-")[1]
+  console.log(userChoice)
   // if the clicked element is not a choice button, do nothing.
   if (userChoice === questions[currentQuestion].correctAnswer) {
-     console.log('correct')
-     score +=5
-     feedback.textContent = "Question:"+(currentQuestion+1)+" is correct. Score :"+ score
-  }else{
+    console.log('correct')
+    score += 5
+    feedback.textContent = "Question:" + (currentQuestion + 1) + " is correct. Score :" + score
+  } else {
     console.log('wrong')
-    feedback.textContent = "Question:"+(currentQuestion+1)+" is wrong. Score :"+ score
+    timeCounter -= 5
+    feedback.textContent = "Question:" + (currentQuestion + 1) + " is wrong. Score :" + score
   }
 
-  if (currentQuestion < questions.length-1) {
+  if (currentQuestion < questions.length - 1) {
     currentQuestion++
     getQuestion()
-  // check if user guessed wrong
+    // check if user guessed wrong
     // penalize time
 
     // display new time on page
@@ -85,16 +89,17 @@ console.log(userChoice)
   // move to next question
 
   // check if we've run out of questions
-    // if so, end the quiz
-    // else, get the next question
+  // if so, end the quiz
+  // else, get the next question
 }
 
 /// FUNCTION TO END THE QUIZ ///
 function quizEnd() {
-  questionsEl.style.display ="none"
-endScreen.style.display ="block"
-timerEl.textContent = timeCounter;
-clearInterval(timerId)
+  questionsEl.style.display = "none"
+  endScreen.style.display = "block"
+  timerEl.textContent = timeCounter;
+  clearInterval(timerId)
+  document.getElementById("final-score").textContent = (score + timeCounter)
   // stop timer
 
   // show end screen
@@ -107,37 +112,46 @@ clearInterval(timerId)
 /// FUNCTION FOR UPDATING THE TIME ///
 function clockTick() {
   // update time
-  timerId = setInterval(function(){
+  timerId = setInterval(function () {
     timerEl.textContent = timeCounter;
-    if(timeCounter > 0){
+    if (timeCounter > 0) {
       timeCounter--;
-    }else{
+    } else {
       quizEnd()
     }
-  },1000)
+  }, 1000)
   // check if user ran out of time
 }
 
 function saveHighscore() {
   // get value of input box - for initials
-
+  var userName = document.getElementById("initials")
+  var previousScore = JSON.parse(localStorage.getItem("codequiz")) || []
+  previousScore.push({
+    user: userName,
+    score: (score + timeCounter)
+  })
+  localStorage.setItem("codequiz", JSON.stringify(previousScore))
+  endScreen.style.display = "none"
+  highscore.style.display = "block"
   // make sure value wasn't empty
-    // get saved scores from localstorage, or if not any, set to empty array
+  // get saved scores from localstorage, or if not any, set to empty array
 
-    // format new score object for current user
+  // format new score object for current user
 
-    // save to localstorage
+  // save to localstorage
 
-    // redirect to next page
+  // redirect to next page
 }
 
 /// CLICK EVENTS ///
-  // user clicks button to submit initials
+// user clicks button to submit initials
 
-  // user clicks button to start quiz
+// user clicks button to start quiz
 
-startButton.addEventListener("click",startQuiz)
+startButton.addEventListener("click", startQuiz)
 answerA.addEventListener("click", questionClick)
 answerB.addEventListener("click", questionClick)
 answerC.addEventListener("click", questionClick)
+submitButton.addEventListener("click", saveHighscore)
   // user clicks on element containing choices
